f13063b00de8cdc3cbe5fdf111d421123bec94c9
Oaz <github@azeau.com>
2018-12-02 11:20:34 +0100

    Day 2

---
+++ src/D02.hs
@@ -0,0 +1,26 @@
+module D02
+    ( boxesChecksum, commonLetters
+    ) where
+
+import Data.List
+import Data.List.Utils
+import qualified Data.HashSet as H
+
+boxesChecksum :: String -> Int
+boxesChecksum = checksum . foldl1 add . map has2or3 . map occurrences . words
+  where
+    checksum (a,b) = a*b
+    add (x,y) (x',y') = (x+x',y+y')
+    has2or3 xs = (boolint $ elem 2 xs, boolint $ elem 3 xs)
+    boolint x = if x then 1 else 0
+    occurrences = map length . group .sort
+
+commonLetters :: String -> String
+commonLetters = common . correctIDs . words
+  where
+    common (xs,ys) = map fst $ filter same $ zip xs ys
+    correctIDs ids = head $ snd $ break differByOne [(x,y) | x <- ids, y <- ids]
+    differByOne (xs,ys) = 1 == (sum $ map distance $ zip xs ys)
+    distance x = if same x then 0 else 1
+    same (a,b) = a==b
+
---
+++ test/D02Spec.hs
@@ -0,0 +1,23 @@
+module D02Spec (spec) where
+
+import Test.Hspec
+import D02
+
+spec :: Spec
+spec = do
+
+    describe "boxes checksum" $ do
+      it "no 2's or 3's" $ boxesChecksum "ab cd" `shouldBe` 0
+      it "2 but no 3's" $ boxesChecksum "aba cd" `shouldBe` 0
+      it "3 but no 2's" $ boxesChecksum "abaca cd" `shouldBe` 0
+      it "one 2 and one 3" $ boxesChecksum "abaca cdc" `shouldBe` 1
+      it "two 2 and one 3" $ boxesChecksum "abaca cdc ghg" `shouldBe` 2
+      it "two 2 and two 3" $ boxesChecksum "abaca cdc ghghg" `shouldBe` 4
+      it "bigger example" $ boxesChecksum "abcdef bababc abbcde abcccd aabcdd abcdee ababab" `shouldBe` 12
+      it "submits answer" $ do input <- readFile "test/D02.txt"; boxesChecksum input `shouldBe` 8610
+
+
+    describe "common letters" $ do
+      it "example" $ commonLetters "abcde fghij klmno pqrst fguij axcye wvxyz" `shouldBe` "fgij"
+      it "submits answer" $ do input <- readFile "test/D02.txt"; commonLetters input `shouldBe` "iosnxmfkpabcjpdywvrtahluy"
+  
