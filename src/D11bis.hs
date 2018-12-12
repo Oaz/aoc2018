4747a1da0a10c915eaa7c5fc197c5aff69166980
Oaz <github@azeau.com>
2018-12-12 01:06:15 +0100

    improve summedAreaTable...

--- src/D11bis.hs
+++ src/D11bis.hs
@@ -12,10 +12,19 @@ powerCells seed = M.matrix 300 300 cellValue
           where rackId = x+10
                 digit n v = mod (div v n) 10
 
-summedAreaTable :: (Num a) => M.Matrix a -> M.Matrix a
-summedAreaTable = M.transpose . sumRows . M.transpose . sumRows
+summedAreaTable' :: (Num a) => M.Matrix a -> M.Matrix a
+summedAreaTable' = M.transpose . sumRows . M.transpose . sumRows
   where sumRows m = L.foldl (\m' r -> M.combineRows r 1 (r-1) m') m [2..(M.nrows m)]
 
+summedAreaTable :: (Num a) => M.Matrix a -> M.Matrix a
+summedAreaTable m = ss
+  where s (1,1) = m M.! (1,1)
+        s (1,y) = (m M.! (1,y)) + (ss M.! (1,y-1))
+        s (x,1) = (m M.! (x,1)) + (ss M.! (x-1,1))
+        s (x,y) = (m M.! (x,y)) + (ss M.! (x,y-1)) + (ss M.! (x-1,y)) - (ss M.! (x-1,y-1)) 
+        ss = M.matrix (M.ncols m) (M.nrows m) s
+
+                
 highest :: Int -> M.Matrix Int -> ((Int,Int),Int)
 highest size sat = L.maximumBy (comparing snd) [((x+1,y+1),sumBySat x y) | x<-range, y<-range]
   where range = [0..(300-size)]
