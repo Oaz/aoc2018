89f8911d39e4218466027379899767831d8628dd
Oaz <github@azeau.com>
2018-12-22 14:25:45 +0100

    Day 20 - part 2

--- src/D20.hs
+++ src/D20.hs
@@ -1,31 +1,55 @@
 module D20
-    ( largestNumberOfDoors
+    ( largestNumberOfDoors, countRoomsAfterDoors
     ) where
 
 import Text.Parsec
 import Data.Functor.Identity
 import Data.Either
+import Data.Complex
+import qualified Data.HashMap.Strict as M
 
-type DoorsParser = ParsecT String () Identity Int
+type Position = Complex Float
+data Navigation = Navigation  { currentPosition :: Position
+                              , pastBranches :: [Position]
+                              , distances :: M.HashMap Position Int } deriving (Show,Eq)
+
+changePosition :: Position -> Navigation -> Navigation
+changePosition p (Navigation cp pb d) = Navigation np pb (M.insert np (shortest (M.lookup np d) (M.lookup cp d)) d)
+  where np=cp+p
+        shortest Nothing (Just n) = n+1
+        shortest (Just n) Nothing = n
+        shortest (Just m) (Just n) = min m (n+1)
+
+enterBranch :: Navigation -> Navigation
+enterBranch (Navigation cp pb d) = Navigation cp (cp:pb) d
+
+tryAlternateBranch :: Navigation -> Navigation
+tryAlternateBranch (Navigation _ pb@(p:_) d) = Navigation p pb d
+
+leaveBranch :: Navigation -> Navigation
+leaveBranch (Navigation _ (p:pb) d) = Navigation p pb d
+
+type RoomParser = ParsecT String Navigation Identity
+
+tryAllPaths :: ParsecT String Navigation Identity Navigation
+tryAllPaths = char '^'
+              >> (many $ choice [ char 'N' >> modifyState (changePosition $ 0:+(-1))
+                                , char 'S' >> modifyState (changePosition $ 0:+1)
+                                , char 'E' >> modifyState (changePosition $ 1:+0)
+                                , char 'W' >> modifyState (changePosition $ (-1):+0)
+                                , char '(' >> modifyState enterBranch
+                                , char '|' >> modifyState tryAlternateBranch
+                                , char ')' >> modifyState leaveBranch ])
+              >> char '$'
+              >> getState
+
+getAllDistances :: String -> [Int]
+getAllDistances = M.elems . distances . fromRight nav . runParser tryAllPaths nav ""
+  where nav = Navigation (0:+0) [] (M.singleton (0:+0) 0)
 
 largestNumberOfDoors :: String -> Int
-largestNumberOfDoors = fromRight 0 . runParser directions () ""
+largestNumberOfDoors = maximum . getAllDistances
 
-directions :: DoorsParser
-directions = do { char '^'
-                ; numberOfDoors <- path
-                ; char '$'
-                ; return numberOfDoors
-                }
+countRoomsAfterDoors :: Int -> String -> Int
+countRoomsAfterDoors threshold = length . filter (((<=)threshold)) . getAllDistances
 
-path :: DoorsParser
-path = do { directions <- many $ oneOf "NSEW"
-          ; followedBy <- option 0 (do { char '('
-                                        ; paths <- path `sepBy` (char '|')
-                                        ; char ')'
-                                        ; followedBy <- option 0 path
-                                        ; return $ (choosePath paths) + followedBy
-                                        })
-          ; return $ (length directions) + followedBy
-          }
-  where choosePath xs = if elem 0 xs then 0 else maximum xs
--- test/D20Spec.hs
+++ test/D20Spec.hs
@@ -16,4 +16,24 @@ spec = do
   describe "part 1" $ do
     it "submits answer" $ do input <- readFile "test/D20.txt"; largestNumberOfDoors input `shouldBe` 3415
     
-
+  describe "count rooms" $ do
+    it "example 1a" $ countRoomsAfterDoors 1 "^WNE$" `shouldBe` 3
+    it "example 1b" $ countRoomsAfterDoors 2 "^WNE$" `shouldBe` 2
+    it "example 1c" $ countRoomsAfterDoors 3 "^WNE$" `shouldBe` 1
+    it "example 2a" $ countRoomsAfterDoors 3 "^ENWWW(NEEE|SSE)$" `shouldBe` 10
+    it "example 2b" $ countRoomsAfterDoors 6 "^ENWWW(NEEE|SSE)$" `shouldBe` 7
+    it "example 2c" $ countRoomsAfterDoors 8 "^ENWWW(NEEE|SSE)$" `shouldBe` 3
+    it "example 2d" $ countRoomsAfterDoors 8 "^ENWWW(NEEE|SSE(EE|N))$" `shouldBe` 6
+    it "example 2e" $ countRoomsAfterDoors 10 "^ENWWW(NEEE|SSE(EE|N))$" `shouldBe` 1
+    it "example 3a" $ countRoomsAfterDoors 6 "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$" `shouldBe` 19
+    it "example 3b" $ countRoomsAfterDoors 8 "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$" `shouldBe` 17
+    it "example 3c" $ countRoomsAfterDoors 11 "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$" `shouldBe` 12
+    it "example 3d" $ countRoomsAfterDoors 15 "^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$" `shouldBe` 7
+    it "example 4a" $ countRoomsAfterDoors 12 "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$" `shouldBe` 23
+    it "example 4b" $ countRoomsAfterDoors 17 "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$" `shouldBe` 12
+    it "example 4c" $ countRoomsAfterDoors 19 "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$" `shouldBe` 7
+    it "example 4d" $ countRoomsAfterDoors 22 "^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$" `shouldBe` 2
+    it "example 5a" $ countRoomsAfterDoors 22 "^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$" `shouldBe` 26
+ 
+  describe "part 2" $ do
+    it "submits answer" $ do input <- readFile "test/D20.txt"; countRoomsAfterDoors 1000 input `shouldBe` 8583
