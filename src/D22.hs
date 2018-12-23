c53a427ec25dee7e7851c833943b84d2fb26a089
Oaz <github@azeau.com>
2018-12-23 16:06:03 +0100

    Day 22 - part 2

--- src/D22.hs
+++ src/D22.hs
@@ -1,21 +1,31 @@
 module D22
-    ( Coordinate(..), Cave(..), mkCave, geologicIndex, erosionLevel, regionType, riskLevel
+    ( Coordinate(..), Cave(..), RegionType(..)
+    , mkCave, geologicIndex, erosionLevel, regionType, riskLevel, shortestPathToTarget
     ) where
 
-import qualified Data.MemoCombinators as MM
+import qualified Data.HashMap.Strict as HM
+import qualified Data.MemoCombinators as MC
+import Data.Graph.Inductive
+import Data.Maybe
+import Data.List
+import Data.Ord
+import Data.Hashable
 
+{- ######### Coordinate ######### -}
 type Coordinate = (Int,Int)
-data Cave = Cave  { depth :: Integer
-                  , target :: Coordinate
-                  , indexesAndLevels :: Int -> Int -> (Integer,Integer) }
 
 rangeTo :: Coordinate -> [Coordinate]
 rangeTo (xm,ym) = [(x,y) | x<-[0..xm], y<-[0..ym]]
 
+{- ######### Cave ######### -}
+data Cave = Cave  { depth :: Integer
+                  , target :: Coordinate
+                  , indexesAndLevels :: Int -> Int -> (Integer,Integer) }
+
 mkCave :: Integer -> Coordinate -> Cave
-mkCave depth target = Cave depth target indexesAndLevels
+mkCave depth target@(xt,yt) = Cave depth target indexesAndLevels
   where calcLevel i = mod (depth + i) 20183
-        indexesAndLevels = MM.memo2 MM.integral MM.integral giel
+        indexesAndLevels = MC.memo2 MC.integral MC.integral giel
         giel 0 0 = (0, calcLevel 0)
         giel x 0 = let i = (toInteger x)*16807
                    in (i, calcLevel i)
@@ -24,7 +34,11 @@ mkCave depth target = Cave depth target indexesAndLevels
         giel x y = let (_,l1) = indexesAndLevels (x-1) y
                        (_,l2) = indexesAndLevels x (y-1)
                        i = l1*l2
-                   in (i, calcLevel i)
+                       i' = if (x,y) == target then 0 else i
+                   in (i', calcLevel i')
+
+rangeFor :: Cave -> [Coordinate]
+rangeFor (Cave _ target _) = rangeTo target
 
 geologicIndex :: Cave -> Coordinate -> Integer
 geologicIndex (Cave _ _ il) (x,y) = let (gi,_) = il x y in gi
@@ -32,9 +46,69 @@ geologicIndex (Cave _ _ il) (x,y) = let (gi,_) = il x y in gi
 erosionLevel  :: Cave -> Coordinate -> Int
 erosionLevel (Cave _ _ il) (x,y) = let (_,el) = il x y in fromInteger $ el
 
-regionType :: Cave -> Coordinate -> Int
-regionType c p = mod (erosionLevel c p) 3
+{- ######### RegionType ######### -}
+data RegionType = Rocky | Wet | Narrow deriving (Show,Eq,Enum)
 
+regionType :: Cave -> Coordinate -> RegionType
+regionType c p = toEnum $ mod (erosionLevel c p) 3
+
+instance Show Cave where 
+  show cave = unlines $ map showRow [0..(snd $ target $ cave)]
+    where showRow y = map (showRegion y) [0..(fst $ target $ cave)]
+          showRegion y x = showRegionType $ regionType cave (x,y)
+          showRegionType Rocky = '.'
+          showRegionType Wet = '='
+          showRegionType Narrow = '|'
+
+{- ######### part 1 : risk level ######### -}
 riskLevel :: Cave -> Int
-riskLevel c@(Cave _ p il) = s - regionType c p
-  where s = sum $ map (regionType c) $ rangeTo p
+riskLevel cave = sum $ map (fromEnum . regionType cave) $ rangeFor cave
+
+{- ######### Tools ######### -}
+data Tool = Torch | Gear | Neither deriving (Show,Eq,Enum)
+instance Hashable Tool where hashWithSalt = hashUsing fromEnum
+
+toolAllowed :: Tool -> RegionType -> Bool
+toolAllowed Gear Narrow = False
+toolAllowed Torch Wet = False
+toolAllowed Neither Rocky = False
+toolAllowed _ _ = True
+
+allTools :: [Tool]
+allTools = [Torch,Gear,Neither]
+
+{- ######### part 2 : path search ######### -}
+infix 6 :@
+data Position  = Coordinate :@ Tool deriving (Eq, Show)
+instance Hashable Position where 
+  hashWithSalt s ((x,y):@t) = s `hashWithSalt` x `hashWithSalt` y `hashWithSalt` t
+
+shortestPathToTarget :: Cave -> Int
+shortestPathToTarget cave@(Cave _ target _) = fromJust $ spLength 1 targetNode graph
+  where graph = mkGraph nodes edges :: Gr Position Int
+        nodes = listNodes cave
+        edges = listEdges cave nodes
+        isTargetNode (n,c:@t) = (t==Torch) && (c==target)
+        targetNode = fst $ fromJust $ find isTargetNode $ nodes
+
+listNodes :: Cave -> [(Node,Position)]
+listNodes cave@(Cave _ (xt,yt) _) = zip [1..] $ concatMap positionsAt $ rangeTo (5*xt, 2*yt)
+  where positionsAt c = [c:@t | t <- allTools, canUseToolAt c t]
+        canUseToolAt c t = toolAllowed t $ regionType cave c
+
+listEdges :: Cave -> [(Node,Position)] -> [(Node,Node,Int)]
+listEdges cave nodes = catMaybes $ map goFromTo $ [(x1,x2) | x1<-nodes, x2<-(neighbors x1)]
+  where neighbors (i,c:@t) = catMaybes [HM.lookup (c':@t') nodeSearch | c'<-neighborCoord c, t'<-allTools]
+        neighborCoord (x,y) = [(x,y), (x+1,y), (x-1,y), (x,y+1), (x,y-1)]
+        nodeSearch = HM.fromList $ map (\n -> (snd n,n)) nodes
+        goFromTo ((i1,n1),(i2,n2)) = changePosition (i1,i2,positionChange n1 n2)
+        changePosition (i1,i2,Just v) = Just (i1,i2,v)
+        changePosition (i1,i2,Nothing) = Nothing
+
+positionChange :: Position -> Position -> Maybe Int
+positionChange p1@(c1:@t1) p2@(c2:@t2)
+  | p1==p2 = Nothing
+  | c1==c2 = Just 7
+  | t1==t2 = let ((x1,y1),(x2,y2))=(c1,c2)
+             in if (abs (x1-x2))+(abs (y1-y2)) == 1 then Just 1 else Nothing
+  | otherwise = Nothing
--- test/D22Spec.hs
+++ test/D22Spec.hs
@@ -4,7 +4,7 @@ import Test.Hspec
 import D22
 
 
-checkAt :: Cave -> Coordinate -> Integer -> Int -> Int -> SpecWith ()
+checkAt :: Cave -> Coordinate -> Integer -> Int -> RegionType -> SpecWith ()
 checkAt c p gi el rt = describe ("example "++(show p)) $ do
                           it "geologic index" $ geologicIndex c p `shouldBe` gi
                           it "erosion level" $ erosionLevel c p `shouldBe` el
@@ -14,10 +14,10 @@ spec :: Spec
 spec = do
 
   let sampleCave = mkCave 510 (10,10)
-  checkAt sampleCave (0,0) 0 510 0
-  checkAt sampleCave (1,0) 16807 17317 1
-  checkAt sampleCave (0,1) 48271 8415 0
-  checkAt sampleCave (1,1) 145722555 1805 2
+  checkAt sampleCave (0,0) 0 510 Rocky
+  checkAt sampleCave (1,0) 16807 17317 Wet
+  checkAt sampleCave (0,1) 48271 8415 Rocky
+  checkAt sampleCave (1,1) 145722555 1805 Narrow
 
   describe "example (10,10)" $ do
     it "risk level" $ riskLevel sampleCave `shouldBe` 114
@@ -25,3 +25,10 @@ spec = do
   let realCave = mkCave 3198 (12,757)
   describe "part 1" $ do
     it "risk level" $ riskLevel realCave `shouldBe` 9659
+  
+  describe "example (10,10)" $ do
+    it "shortest path" $ shortestPathToTarget sampleCave `shouldBe` 45
+  
+  describe "part 2" $ do
+    it "shortest path" $ shortestPathToTarget realCave `shouldBe` 1043
+     
